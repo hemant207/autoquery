6fcfa74e35e025b0faee435c9b9bbefccb548f02
hemant207 <hemantmangukiya93@gmail.com>
2023-09-27 17:37:26 +0100

    added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app  = express();
+const { sendMessage, getTextMessageInput } = require("./messagehelper");
 
+const axios = require('axios');
 
 app.use(express.json());
 app.get('/',(req,res)=>{
@@ -22,18 +24,40 @@ app.get('/webhooks', (req, res) => {
       res.status(403).send('Forbidden');
     }
   });
-  
 
+  app.post('/webhooks', (req, res) => {
+    // Extract the incoming message and relevant context from the payload
+    const message = req.body.message.text; // Assuming WhatsApp message text is in "message.text"
+    const sender = req.body.sender.id; // Assuming WhatsApp sender ID is in "sender.id"
+    const timestamp = req.body.timestamp; // Assuming message timestamp is in "timestamp"
+  
+    // Implement your logic to process and respond to the incoming WhatsApp message
+    // You can also store this information for conversation management
+  
+    // Example: Log the incoming message
+    console.log(`Received message from ${sender} at ${new Date(timestamp)}: ${message}`);
+  
+    // Example: Send a response (replace with your actual response logic)
+    const responseMessage = 'Thank you for your message. We will get back to you soon.';
 
-app.post('/whatsapp/getmsg', (req, res) => {
-    console.log("Message received:");
-    console.log(req.body); // Assuming WhatsApp sends the message payload in the request body
-    // Handle the incoming WhatsApp message here
-  
-    // Send a response back to WhatsApp if needed
-  
-    res.status(200).send("Message received and processed.");
+    var data = getTextMessageInput(process.env.RECIPIENT_WAID, responseMessage);
+    console.log(data);
+    sendMessage(data)
+      .then(function (res) {
+        res.redirect('/');
+        res.sendStatus(200);
+        return;
+      })
+      .catch(function (error) {
+        console.log(error);
+        console.log(error.response.data);
+        res.sendStatus(500);
+        return;
+      });
+    // Respond to the incoming webhook request
+    res.status(200).send('Received message');
   });
+  
 
 app.listen(3000,()=>{
     console.log('app is running on 3000')

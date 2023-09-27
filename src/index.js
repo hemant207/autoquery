899ee03991f974589ab6bf4c60c2cff189196fe5
hemant207 <hemantmangukiya93@gmail.com>
2023-09-27 16:52:11 +0100

    added

--- src/index.js
+++ src/index.js
@@ -7,14 +7,16 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/whatsapp/getmsg', (req, res) => {
-    console.log("Message received:");
-    console.log(req.body); // Assuming WhatsApp sends the message payload in the request body
-    // Handle the incoming WhatsApp message here
-  
-    // Send a response back to WhatsApp if needed
-  
-    res.status(200).send("Message received and processed.");
-  });
+    if (req.query.challenge) {
+        // Respond to the challenge request by echoing the challenge value
+        res.status(200).send(req.query.challenge);
+      } else {
+        // Handle the incoming WhatsApp message here
+        console.log(req.body); // Assuming WhatsApp sends the message payload in the request body
+        // Implement your logic to process and respond to the message
+        res.status(200).send("Message received and processed.");
+      }
+})
 
 
 app.post('/whatsapp/getmsg', (req, res) => {

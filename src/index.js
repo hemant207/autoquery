9083a58f5a0fe24f33e4425d5f0313f75d4ab6bc
hemant207 <hemantmangukiya93@gmail.com>
2023-09-27 16:49:06 +0100

    added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ app.get('/',(req,res)=>{
     res.send("running............")
 })
 
+app.get('/whatsapp/getmsg', (req, res) => {
+    console.log("Message received:");
+    console.log(req.body); // Assuming WhatsApp sends the message payload in the request body
+    // Handle the incoming WhatsApp message here
+  
+    // Send a response back to WhatsApp if needed
+  
+    res.status(200).send("Message received and processed.");
+  });
+
+
 app.post('/whatsapp/getmsg', (req, res) => {
     console.log("Message received:");
     console.log(req.body); // Assuming WhatsApp sends the message payload in the request body
